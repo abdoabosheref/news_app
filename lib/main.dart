b81083d16541569ef91f6315d66cc4eb52b113f7
Abdo <abdoabosheref@gmail.com>
2026-04-25 15:20:33 +0300

    init project

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+// import 'package:news_app/core/utils/app_routes.dart';
+// import 'package:news_app/ui/home_screen/home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      // debugShowCheckedModeBanner: false,
+      //  initialRoute: AppRoutes.homeScreen,
+      // routes: {
+      //   AppRoutes.homeScreen : (context) => HomeScreen(),
+      // },
+
+    );
+  }
+}
